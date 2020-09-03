5fa3339d15f4664697c8febbaf8b7d056506e57d
HueiFeng <695979933@qq.com>
2020-09-03 10:09:15 +0800

    delete files

--- src/Bundling/LYearThemeGlobalStyleContributor.cs
+++ src/Bundling/LYearThemeGlobalStyleContributor.cs
@@ -8,7 +8,6 @@ namespace EasyAbp.Abp.AspNetCore.Mvc.UI.Theme.LYear.Bundling
         {
             context.Files.RemoveAll(x => x.Contains("bootstrap.css"));
             context.Files.Add("/themes/lyear/assets/css/bootstrap.min.css");
-            context.Files.Add("/themes/lyear/assets/modules/fontawesome/css/all.css");
             context.Files.Add("/themes/lyear/assets/css/materialdesignicons.min.css");
             context.Files.Add("/themes/lyear/assets/css/style.min.css");
             context.Files.Add("/themes/lyear/assets/css/custom.css");
